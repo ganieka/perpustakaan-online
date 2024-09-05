f7757da873b20874a7331a14cd44e57c0af61558
ganisantoso <ganisantosoeka@gmail.com>
2024-09-05 22:42:59 +0700

    update

--- src/component/DetailBuku.jsx
+++ src/component/DetailBuku.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const DetailBuku = (params) => {
     const {router, buku} = params
@@ -11,13 +12,47 @@ const DetailBuku = (params) => {
                         <div className="detail-head">
                             <div className="image-container">
                                 <Image src={buku[0].image} alt="cover buku" width={170} height={210}/>
-                                <button className="button">
-
-                                </button>
+                                <Link href={`/`}>
+                                    <button className="read-button">
+                                        Baca Buku
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="detail-head-right">
+                                <div className="detail-head-1">
+                                    <div>
+                                        <h2>{buku[0].nama}</h2>
+                                        <p>by {buku[0].penulis}</p>
+                                        <p><span>Kategori: </span>{buku[0].kategori}</p>
+                                    </div>
+                                    <div>
+                                        <h1>0.0</h1>
+                                        <p style={{fontSize: 24}}>Rating</p>
+                                    </div>
+                                </div>
+                                <div className="detail-head-2">
+                                    <div>
+                                        <h5>Tanggal Terbit</h5>
+                                        2024
+                                    </div>
+                                    <div>
+                                        <h5>Penerbit</h5>
+                                        Standard Ebooks
+                                    </div>
+                                    <div>
+                                        <h5>Bahasa</h5>
+                                        Inggris
+                                    </div>
+                                    <div>
+                                        <h5>Halaman</h5>
+                                        2024
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="detail-body">
-
+                            <h3>Sinopsis</h3>
+                            <p className="sinopsis">{buku[0].deskripsi}</p>
                         </div>
                     </div>
                 </section>
